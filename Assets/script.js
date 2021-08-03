11d4f6766de8167cbe5f25ec7a50249ca51d7069
Hazelton Chrome <rdupont1623@gmail.com>
2021-08-03 18:20:29 -0400

    FINALLY got it to work!!!

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,6 @@
 
+let saveEntry = document.querySelector("#save");
+
 // Running Clock Function
 let Clock = function() {
 document.querySelector("#nowTime").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -28,12 +30,37 @@ $.each(hourLine, function(i, hour){
         }
 });
 
-/*when the button is pressed, the function should
-clear existing local storage 
-(window.localStorage.removeItem('name of item'))
-and replace in that item's location a new string that
-was entered by the user
-*/
+$(".save").on("click", function () {
+    // Get nearby values of the description in JQuery
+    let text = $(this).siblings(".textbox").val();
+    var time = $(this).parent().attr("id");
+
+    // Saveing to local storage
+    localStorage.setItem(time, text);
+})
+
+
+$("#hour6 .textbox").val(localStorage.getItem("hour6"));
+$("#hour7 .textbox").val(localStorage.getItem("hour7"));
+$("#hour8 .textbox").val(localStorage.getItem("hour8"));
+$("#hour9 .textbox").val(localStorage.getItem("hour9"));
+$("#hour10 .textbox").val(localStorage.getItem("hour10"));
+$("#hour11. textbox").val(localStorage.getItem("hour11"));
+$("#hour12. textbox").val(localStorage.getItem("hour12"));
+$("#hour13 .textbox").val(localStorage.getItem("hour13"));
+$("#hour14 .textbox").val(localStorage.getItem("hour14"));
+$("#hour15 .textbox").val(localStorage.getItem("hour15"));
+$("#hour16 .textbox").val(localStorage.getItem("hour16"));
+$("#hour17 .textbox").val(localStorage.getItem("hour17"));
+$("#hour18 .textbox").val(localStorage.getItem("hour18"));
+$("#hour19 .textbox").val(localStorage.getItem("hour19"));
+$("#hour20 .textbox").val(localStorage.getItem("hour20"));
+$("#hour21 .textbox").val(localStorage.getItem("hour21"));
+$("#hour22 .textbox").val(localStorage.getItem("hour22"));
+$("#hour23 .textbox").val(localStorage.getItem("hour23"));
+$("#hour24 .textbox").val(localStorage.getItem("hour24"));
+
+
 
 
 Clock();
